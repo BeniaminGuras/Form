832880970ee5c49ee8b4338b9b7ed66a385137f8
Beniamin Guras <gurasbeniamin@gmail.com>
2022-06-10 23:08:58 +0200

    fix formating

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -7,38 +7,38 @@ import PopUpWindow from '../PopUpWindow/PopUpWindow';
 
 const Form = () => {
 
-    const form = useForm(); 
+  const form = useForm();
 
-    return(
-        <div className={styles.formContainer}>
-            <PopUpWindow error={form.error} errorMessage={form.errorMessage} setError={form.setError} /> 
-            <form onSubmit={form.onSubmit}>
-                <div className={styles.formOpiton}>
-                    <p className={styles.title}>Name:</p>
-                    <input type='text' value={form.name} onChange={e => form.setName(e.target.value)}></input>
-                </div>
-                <div className={styles.formOpiton}>
-                    <p className={styles.title}>Preparation Time:</p>
-                    <input type='time' value={form.duration} onChange={e => form.setDuration(e.target.value)} step="1"></input>
-                </div>
-                <div className={styles.formOpiton}>
-                    <p className={styles.title}>Type:</p>
-                    <select onChange={e => {
-                        form.setDish(e.target.value);
-                        form.resetAllConditionalValues();
-                    }}>
-                        <option value={PIZZA}>{PIZZA}</option>
-                        <option value={SOUP}>{SOUP}</option>
-                        <option value={SANDWICH}>{SANDWICH}</option>
-                    </select>
-                </div>
-                    <ConditionalInput dish={form.dish} states={form.conditionalStates} />
-                    <div className={styles.buttonContainer}>
-                        <button type="submit">Submit</button>
-                    </div>
-            </form>
+  return(
+    <div className={styles.formContainer}>
+      <PopUpWindow error={form.error} errorMessage={form.errorMessage} setError={form.setError} />
+      <form onSubmit={form.onSubmit}>
+        <div className={styles.formOpiton}>
+          <p className={styles.title}>Name:</p>
+          <input type='text' value={form.name} onChange={e => form.setName(e.target.value)}></input>
         </div>
-    )
+        <div className={styles.formOpiton}>
+          <p className={styles.title}>Preparation Time:</p>
+          <input type='time' value={form.duration} onChange={e => form.setDuration(e.target.value)} step="1"></input>
+        </div>
+        <div className={styles.formOpiton}>
+          <p className={styles.title}>Type:</p>
+          <select onChange={e => {
+            form.setDish(e.target.value);
+            form.resetAllConditionalValues();
+          }}>
+            <option value={PIZZA}>{PIZZA}</option>
+            <option value={SOUP}>{SOUP}</option>
+            <option value={SANDWICH}>{SANDWICH}</option>
+          </select>
+        </div>
+          <ConditionalInput dish={form.dish} states={form.conditionalStates} />
+          <div className={styles.buttonContainer}>
+            <button type="submit">Submit</button>
+          </div>
+      </form>
+    </div>
+  )
 }
 
 export default Form;
